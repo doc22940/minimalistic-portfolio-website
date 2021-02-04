0e3e90346a3262f0a38b23451eb868a76243bac5
Acampbell <57328920+doc22940@users.noreply.github.com>
2021-02-04 12:31:26 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -15,14 +15,7 @@ function About() {
                    {/*ABOUT*/}
                   <MDBCol style={{marginTop: 25}} className="col-12 col-sm-12 col-lg-5 text-center">
                    <MDBAnimation reveal type="fadeInRight" duration="2s">
-        <h4><strong style={{marginBottom: 25}}>Hi, I'm Wonder Woman</strong></h4><p className="text-center">Im the beautifulfully talented woman every girl wants to be..
-        Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis
-        piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium
-        purus sit amet fermentum.  Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis
-        piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium
-        purus sit amet fermentum.Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum.
-        Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis
-        piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere.</p>
+        <h4><strong style={{marginBottom: 25}}>Hi, I'm Wonder Woman</strong></h4><p className="text-center">Im the beautifulfully talented woman every girl wants to be..</p>
          </MDBAnimation>
         </MDBCol>
       </MDBRow>
